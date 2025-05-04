2a72feeb496e141134c5b114b067ae154fa3ff51
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2025-05-05 01:42:22 +0300

    Touchpad Toggle script

---
+++ touchpadToggleForX11.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# IMPORTANT NOTE! : If you are using Wayland, DON'T USE THIS! Instead; if you're using KDE or Cinnamon, use
+# their touchpad toggle. If you're using GNOME, install "Touchpad On Off" extension from the extension manager.
+
+# But, if you are using X11 follow these steps :
+# 1. install xinput by typing "sudo apt install xinput" or "sudo dnf install xinput".
+# 2. restart your pc (just in case).
+# 3. type "xinput list" at the terminal and see your touchpad id and write your touchpad id instead of 14.
+# My touchpad id is 14 according to xinput (look at yours even if you changed distros).
+
+enabled=$(xinput --list-props 14 | grep "Device Enabled" | awk '{print $NF}')
+
+if [[ "$enabled" == "1" ]]; then
+    xinput --disable 14
+else
+    xinput --enable 14
+fi
