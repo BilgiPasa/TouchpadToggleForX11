1c93f758c603f725c59a86a57b9ffb033b472b6e
BilgiPasa <bilgicaglarbozkurt@gmail.com>
2025-12-28 18:23:00 +0300

    Updating README and updating the script's comments

--- touchpadToggleForX11.sh
+++ touchpadToggleForX11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is a touchpad toggle script for Debian and Fedora based distros if you are using X11.
+# This is a touchpad toggle script for X11 that uses xinput.
 # This touchpad toggle script can be used with GNOME's settings app for to execute the command with a shortcut.
 # This code is not fully written by me. I found the code on the internet (I don't remember when or where).
 # I might have changed the code or just took this part of the code. I don't remember exacty.
@@ -9,11 +9,11 @@
 # touchpad toggle. If you're using GNOME, install "Touchpad On Off" extension from the extension manager.
 # (That extension is not mine. I recommend it because I have used it.)
 
-# But if you are using X11, follow these steps:
-#   1. install xinput by typing "sudo apt install xinput" or "sudo dnf install xinput".
-#   2. restart your pc (just in case).
-#   3. type "xinput list" at the terminal and see your touchpad id and write your touchpad id instead of 14.
-#   My touchpad id is 14 according to xinput (look at yours even if you changed distros).
+# To use this script, follow these steps:
+#   1. Install xinput with "sudo apt install xinput" or "sudo dnf install xinput" or with another command.
+#   2. Restart your pc (just in case).
+#   3. Type "xinput list" at the terminal and see your touchpad ID and write your touchpad ID instead of 14.
+#   My touchpad ID was 14 according to xinput. Because of that, I wrote 14.
 
 enabled=$(xinput --list-props 14 | grep "Device Enabled" | awk '{print $NF}')
 
